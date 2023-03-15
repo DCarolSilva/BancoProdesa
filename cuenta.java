635071cadf5daafa4c51c437bdd3156dc615040c
cristhiammercado <cristhiam_o@yahoo.com>
2023-03-15 17:35:09 -0600

    first commit

---
+++ cuenta.java
@@ -0,0 +1,12 @@
+public class cuenta {
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+    private Double saldo;
+
+}
