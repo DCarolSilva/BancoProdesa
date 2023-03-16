4267ecb9d81022029c7b51fefb2598449cb2d0f3
JAreyes98 <josuereyeschevy@gmail.com>
2023-03-16 16:28:51 -0600

    Avances en la clase principal

--- app.java
+++ app.java
@@ -6,35 +6,38 @@ public class app {
          cuenta c = new cuenta();
          c.setSaldo(1000d);
 
-         System.out.printf("\nSaldo inicial: %.2f", c.getSaldo());
+         System.out.printf("\nSaldo inicial: C$%.2f", c.getSaldo());
 
          boolean running = true;
          while (running) {
-             System.out.print("\n\n==========================Opciones==========================");
+             System.out.println("\n\n==========================Opciones==========================");
              System.out.println("1. Credito moneda local");
              System.out.println("2. Credito dolar");
-             System.out.println("3. Credito moneda local");
-             System.out.println("4. Credito dolar");
+             System.out.println("3. Debito moneda local");
+             System.out.println("4. Debito dolar");
              System.out.println("5. Salir");
 
              switch (leerInt("Digite su opcion", bf)) {
                  case 1:
-                     //OPCION 1
+                     //OPCION 1 - CREDITO MONEDA LOCAL
+                     new CreditoCordobas().realizarCreditoCordobas(c, leerDouble("Monto a acreditar", bf));
                      break;
                  case 2:
-                     //OPCION 2
+                     //OPCION 2 - CREDITO DOLAR
                      break;
                  case 3:
-                     //OPCION 3
+                     //OPCION 3 - DEBITO MONEDA LOCAL
                      break;
                  case 4:
-                     //OPCION 4
+                     //OPCION 4 - DEBITO DOLAR
                      break;
                  case 5:
                      running = false;
                      System.out.println("\n\nBye");
                      break;
              }
+
+             System.out.printf("\n\t*** Saldo actual: C$%.2f ***\n", c.getSaldo());
          }
 
 
@@ -46,11 +49,11 @@ public class app {
          Double value = null;
          while (noPass) {
              try {
-                 System.out.println(mensaje.concat(": "));
+                 System.out.print(mensaje.concat(": "));
                  value = Double.parseDouble(in.readLine());
                  noPass = false;
              }catch (IOException e) {
-                 System.err.println("\n\tEl argumento no es un número");
+                 System.out.println("\n\tEl argumento no es un número");
              }
          }
          return value;
@@ -60,11 +63,11 @@ public class app {
         Integer value = null;
         while (noPass) {
             try {
-                System.out.println(mensaje.concat(": "));
+                System.out.print(mensaje.concat(": "));
                 value = Integer.parseInt(in.readLine());
                 noPass = false;
-            }catch (IOException e) {
-                System.err.println("\n\tEl argumento no es un número");
+            }catch (NumberFormatException | IOException e) {
+                System.out.println("\n\tEl argumento no es un número");
             }
         }
         return value;
